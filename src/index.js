e04058537e5304e94b7a26077a5a4d3fc57b10d6
BartlomiejPop <bartlomiejpoprawa01@gmail.com>
2023-06-02 18:55:58 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 console.log('circles');
 
-import { setRankingScore } from './firebase.js';
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
+import { getDatabase, ref, set, child, get, update } from 'firebase/database';
+import { getAnalytics } from 'firebase/analytics';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyApgtQH9TpXuGobxQfeZcPYiF2US1oLQrA',
+  authDomain: 'circles-ranking.firebaseapp.com',
+  databaseURL: 'https://circles-ranking-default-rtdb.firebaseio.com',
+  projectId: 'circles-ranking',
+  storageBucket: 'circles-ranking.appspot.com',
+  messagingSenderId: '887086823949',
+  appId: '1:887086823949:web:13d21ff9c3739fb70b7344',
+  measurementId: 'G-DHEYVW0W7P',
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase();
+const auth = getAuth(app);
+const dbRef = ref(getDatabase());
+const emailInputEl = document.querySelector('.email-input');
+const passwordInputEl = document.querySelector('.password-input');
+const logInBtn = document.querySelector('.login-btn');
+let user;
+let id;
+let loginEmail;
+let loginPassword;
+console.log('firebase');
 
 const containerEl = document.querySelector('.container');
 const circleEl = document.querySelector('.circle');
@@ -57,6 +92,7 @@ const gameOver = record => {
   finalScoreEl.textContent = `Time's up! Your score is: ${scorePoints}`;
   scorePoints = 0;
   scoreEl.innerText = scorePoints;
+  timerBgEl.style.display = 'none';
   clearInterval(bgInterval);
   if (localStorage.getItem('NewRecord') == 0) {
     localStorage.setItem('NewRecord', record);
@@ -220,3 +256,73 @@ setColorMode();
 // // // // // // // // // // // // // //
 
 // // // // // // // // // // // // // //
+
+onAuthStateChanged(auth, currentUser => {
+  if (currentUser) {
+    user = currentUser;
+    // logged(currentUser.email);
+  } else {
+    // loggedOut();
+  }
+});
+
+document.getElementById('log-btn').addEventListener('click', function () {
+  loginEmail = document.getElementById('login-email').value;
+  loginPassword = document.getElementById('login-password').value;
+
+  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    .then(userCredential => {
+      user = userCredential.user;
+      Notify.success(`Succesfully logged in as ${user}`, {
+        timeout: 1000,
+      });
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      Notify.failure(`${errorMessage}`, {
+        timeout: 1000,
+      });
+    });
+});
+
+document.getElementById('register-btn').addEventListener('click', function () {
+  loginEmail = document.getElementById('login-email').value;
+  loginPassword = document.getElementById('login-password').value;
+
+  createUserWithEmailAndPassword(auth, loginEmail, loginPassword)
+    .then(userCredential => {
+      user = userCredential.user;
+      Notify.success(`Succesfully registered! Now log in`, {
+        timeout: 1000,
+      });
+      console.log('registered');
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      Notify.failure(`${errorMessage}`, {
+        timeout: 1000,
+      });
+    });
+});
+
+function setRankingScore(record) {
+  console.log(record);
+  // get(child(dbRef, user.id + '/' + 'record'))
+  //   .then(snapshot => {
+  //     if (snapshot.exists()) {
+  //       const updates = {};
+  //       updates[user.id + '/' + 'record'] = {
+  //         record: record,
+  //       };
+  //       update(ref(db), updates);
+  //     } else {
+  //       set(ref(db, user.id + '/' + 'record'), { record: record });
+  //     }
+  //   })
+  //   .catch(error => {
+  //     const errorMessage = error.message;
+  //     Notify.failure(`${errorMessage}`, {
+  //       timeout: 1000,
+  //     });
+  //   });
+}
